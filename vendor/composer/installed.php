a25bb978e19cd8ad39456f52a99d7ea93d891de8
Romain (Rubilmax) Milon <romain@morpho.xyz>
2026-07-12 14:54:24 +0200

    Package production dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/fulltextsearch',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5fd89c70fc24a6f5e4d0db5e44f900f306760a32',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bamarni/composer-bin-plugin' => array(
+            'pretty_version' => '1.9.1',
+            'version' => '1.9.1.0',
+            'reference' => '641d0663f5ac270b1aeec4337b7856f76204df47',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../bamarni/composer-bin-plugin',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nextcloud/fulltextsearch' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5fd89c70fc24a6f5e4d0db5e44f900f306760a32',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
